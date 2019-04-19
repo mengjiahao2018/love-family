694301b756619c2f1f7502d40fecf04dff77ab20
mengjiahao2018 <meng_jiahao@163.com>
2019-04-19 13:25:27 +0800

    repair

--- love-family/src/main/java/com/love/family/business/loginManage/dao/LoginDaoImpl.java
+++ love-family/src/main/java/com/love/family/business/loginManage/dao/LoginDaoImpl.java
@@ -1,28 +1,27 @@
 package com.love.family.business.loginManage.dao;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections.map.HashedMap;
 import org.springframework.stereotype.Repository;
 
 import com.love.family.business.loginManage.entity.LoginUser;
 import com.love.family.dao.base.BaseHibernate4QueryDao;
 
 @Repository
-public class LoginDaoImpl extends BaseHibernate4QueryDao<LoginUser> implements
-		LoginDao {
+public class LoginDaoImpl extends BaseHibernate4QueryDao<LoginUser> implements LoginDao {
 
 	@Override
-	public boolean login(String loginName, String password){
-		Map<String, Object> params = new HashedMap();
+	public boolean login(String loginName, String password) {
+		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("loginName", loginName);
 		params.put("password", password);
 		StringBuffer buffer = new StringBuffer();
 		buffer.append("from LoginUser where loginName= :loginName and password= :password");
 		try {
 			List<LoginUser> loginUserList = super.findEntityObjects(buffer.toString(), params);
-			if(loginUserList!=null&&loginUserList.size()>0){
+			if (loginUserList != null && loginUserList.size() > 0) {
 				return true;
 			}
 		} catch (Exception e) {
@@ -44,11 +43,11 @@ public class LoginDaoImpl extends BaseHibernate4QueryDao<LoginUser> implements
 	public LoginUser findLoginUserByLoginName(String loginName) {
 		StringBuffer buffer = new StringBuffer();
 		buffer.append("from LoginUser where loginName= :loginName");
-		Map<String, Object> params = new HashedMap();
+		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("loginName", loginName);
 		try {
 			List<LoginUser> loginUserList = super.findEntityObjects(buffer.toString(), params);
-			if(loginUserList!=null&&loginUserList.size()>0){
+			if (loginUserList != null && loginUserList.size() > 0) {
 				return loginUserList.get(0);
 			}
 		} catch (Exception e) {
