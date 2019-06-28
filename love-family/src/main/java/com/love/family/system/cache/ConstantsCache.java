89c09daeb7a8d417272943c3df1d14c994970a58
mengjiahao2018 <meng_jiahao@163.com>
2019-06-28 15:49:09 +0800

    repair cache code

--- love-family/src/main/java/com/love/family/system/cache/ConstantsCache.java
+++
@@ -1,5 +0,0 @@
-package com.love.family.system.cache;
-
-public class ConstantsCache {
-	public static final String LOVE_FAMILY_MENU = "love.family.menu";
-}
--- love-family/src/main/java/com/love/family/system/cache/SimpleCacheManager.java
+++
@@ -1,8 +0,0 @@
-package com.love.family.system.cache;
-
-import java.io.Serializable;
-
-public interface SimpleCacheManager {
-
-	<K extends Serializable, V extends Object> SimpleCache<K,V> getCache(String cacheName);
-}
